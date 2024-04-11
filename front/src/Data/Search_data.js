d8e7da54562df9a1e8c27630841bc99ea51ddcfb
mohammadAinia <mmyyttt@gmail.com>
2024-04-12 00:33:17 +0300

    nn

--- front/src/Data/Search_data.js
+++
@@ -1,6 +0,0 @@
-
-const Search_data = [
-    {id:"one" , title:"Search Animal" , placeholder:"Enter Name Animal" ,value:"Search" ,href:"/Animal" , a:"Back" },
-]
-
-export default Search_data
--- front/src/Pages/Validation.js
+++
@@ -1,4 +0,0 @@
-// function Validation (password , confirm){
-//     //  رقم واحة على الاقل و حرف صغيرو كبير 
-//     const password_pattren = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
-// }
