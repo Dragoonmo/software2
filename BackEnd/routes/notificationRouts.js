b5bed8378563282f6f3c863186741eea044b2c1c
mohammadAinia <mmyyttt@gmail.com>
2024-04-12 00:49:34 +0300

    x

--- BackEnd/routes/notificationRouts.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const router=express.Router()
-const noti=require("../controller/notifications")
-
-
-router.get('/notifications',noti.show_noti)
-router.post('/check_n/:id',noti.check_not)
-module.exports=router
